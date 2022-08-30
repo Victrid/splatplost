f4e6136becc775c3642ff14b16da895c43bcf88e
Weihao Jiang <weihau.chiang@gmail.com>
2022-08-30 18:28:17 +0800

    This would fix #10. Documentation needed to instruct how to use.

--- print.py
+++ print.py
@@ -1,98 +1,85 @@
 #!/usr/bin/env python3
 
-import asyncio
-import os
-import sys
+from time import sleep
+from typing import Type
+
 import tqdm
 
-from aioconsole import ainput
-
-from joycontrol.controller import Controller
-from joycontrol.controller_state import ControllerState, button_push, button_press, button_release
-from joycontrol.memory import FlashMemory
-from joycontrol.protocol import controller_protocol_factory
-from joycontrol.server import create_hid_server
-
-delay_factor = 0.08
-
-async def loader(recon):
-    if not os.geteuid() == 0:
-        raise PermissionError('Script must be run as root!')
-    transport, protocol, recon_addr = await create_hid_server(controller_protocol_factory(Controller.from_arg("PRO_CONTROLLER"), spi_flash=FlashMemory()),
-                                                              reconnect_bt_addr=recon,
-                                                              ctl_psm=17,
-                                                              itr_psm=19)
-    controller_state = protocol.get_controller_state()
-    with open("./.reconnect", "w+") as f:
-        f.write(recon_addr)
-    return transport, controller_state
+from libnxctrl.bluetooth import get_backend
+from libnxctrl.wrapper import Button, NXWrapper
 
 
-async def reset(controller_state, left=True, up=True):
-    if left:
-        await button_push(controller_state, 'left', sec=5)
-    else:
-        await button_push(controller_state, 'right', sec=5)
-    if up:
-        await button_push(controller_state, 'up', sec=5)
-    else:
-        await button_push(controller_state, 'down', sec=5)
+def reset(connection: NXWrapper, direction=None):
+    direction_map = {
+        'up':    Button.DPAD_UP,
+        'down':  Button.DPAD_DOWN,
+        'left':  Button.DPAD_LEFT,
+        'right': Button.DPAD_RIGHT,
+        }
+    if direction is None:
+        direction = ['left', 'up']
+    for d in direction:
+        connection.button_hold(direction_map[d], duration_ms=5000)
 
 
-async def printcf(order_list: list, recon_addr):
-    # waits until controller is fully connected
-    if recon_addr is not None:
-        print("You've already paired with a switch and is now connecting"
-        " to it. To reconnect, please remove .reconnect file in the folder."
-        " (This file is ususally a hidden one)")
-    else:
-        print("Open the pairing menu on switch.")
-    transport, controller_state = await loader(recon_addr)
-    await controller_state.connect()
+def plot(order_list: list[str], backend: Type[NXWrapper], delay_ms: int = 100, press_duration_ms: int = 100):
+    connection = backend(press_duration_ms=press_duration_ms)
+    print("Open the pairing menu on switch.")
+    connection.connect()
+
     t = "Placeholder"
-    while t != "":
-        t = await ainput(prompt="Press <enter> to draw, or some words ending "
-        "with <enter> to press the A on the pairing menu.")
-        await button_press(controller_state, "a")
-
-    await asyncio.sleep(1)
+    while t.strip() != "":
+        t = input("Press <enter> to draw, or some words ending "
+                  "with <enter> to press the A on the pairing menu."
+                  )
+        connection.button_press(Button.A)
 
     # Goto (0,0) point
-    await reset(controller_state)
+    reset(connection, ['left', 'up'])
 
     # Clear
-    await button_push(controller_state, 'minus')
-    await asyncio.sleep(1)
+    connection.button_press(Button.MINUS)
+    sleep(0.5)
 
     for order in tqdm.tqdm(order_list):
-        if order.strip('\n') == "lu":
-            await reset(controller_state, left=True, up=True)
-            await asyncio.sleep(delay_factor)
-            continue
-        if order.strip('\n') == "ru":
-            await reset(controller_state, left=False, up=True)
-            await asyncio.sleep(delay_factor)
-            continue
-        if order.strip('\n') == "ld":
-            await reset(controller_state, left=True, up=False)
-            await asyncio.sleep(delay_factor)
-            continue
-        if order.strip('\n') == "rd":
-            await reset(controller_state, left=False, up=False)
-            await asyncio.sleep(delay_factor)
-            continue
-        await button_push(controller_state, order.strip('\n'), sec=0.08)
-        await asyncio.sleep(delay_factor)
-    await transport.close()
+        order = order.strip()
+        reset_map = {
+            'lu': lambda: reset(connection, ['left', 'up']),
+            'ru': lambda: reset(connection, ['right', 'up']),
+            'ld': lambda: reset(connection, ['left', 'down']),
+            'rd': lambda: reset(connection, ['right', 'down']),
+            }
+        button_map = {
+            "up":    Button.DPAD_UP,
+            "down":  Button.DPAD_DOWN,
+            "left":  Button.DPAD_LEFT,
+            "right": Button.DPAD_RIGHT,
+            "a":     Button.A,
+            "b":     Button.B,
+            "x":     Button.X,
+            "y":     Button.Y,
+            }
+        if order in reset_map:
+            reset_map[order]()
+        elif order in button_map:
+            connection.button_press(button_map[order])
+        sleep(delay_ms / 1000)
+
+    connection.disconnect()
+
 
 if __name__ == "__main__":
-    try:
-        with open("./.reconnect") as f:
-            recon_addr = f.read()
-    except FileNotFoundError:
-        recon_addr = None
-    with open(sys.argv[1]) as f:
-        orders = f.readlines()
-        if len(sys.argv) >= 3:
-            delay_factor = float(sys.argv[2])
-        asyncio.run(printcf(orders, recon_addr))
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Draw a plot on the switch.')
+    parser.add_argument('--backend', choices=['nxbt', 'poohl', 'mart1no'], default='nxbt',
+                        help='The backend to use.'
+                        )
+    parser.add_argument('--delay_time_ms', type=int, default=100, help='The delay time between each step.')
+    parser.add_argument('--press_duration_ms', type=int, default=100, help='The duration of each step.')
+    parser.add_argument('--order', required=True, help='The order file.')
+
+    args = parser.parse_args()
+    back = get_backend(args.backend)
+    with open(args.order) as f:
+        plot(f.readlines(), back, args.delay_time_ms, args.press_duration_ms)
